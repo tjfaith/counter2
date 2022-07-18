81cac3701fec55dbe07dd00f09b316ec83f7c819
Tejiri Faith <tjfaithpro@gmail.com>
2022-07-18 19:20:57 +0100

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let delCount = 5;
 let counter = null;
 
+// Function to handle delete 
 function deleteCount() {
   document.getElementById("delBtn").disabled = true;
   counter = setInterval(function () {
@@ -14,14 +15,17 @@ function deleteCount() {
   }, 1000);
 }
 
+// Function to cancel delete
 function cancelDelete() {
   clearInterval(counter);
   document.getElementById("delBtn").disabled = false;
   delCount = 5;
+  document.getElementById("msg").innerHTML =""
   document.getElementById("counter").innerHTML = delCount;
 }
 
-let b = 3;
+// function to make text blink
+let b = 5;
 function blinkText() {
   let deleteText = document.getElementById("msg");
   deleteText.innerHTML = deleteText.innerHTML == "DELETED" ? "" : "DELETED";
@@ -29,6 +33,29 @@ function blinkText() {
     document.getElementById("msg").innerHTML = "DELETED";
   } else {
     b--;
-    setTimeout("blinkText()", 1000);
+    setTimeout("blinkText()", 800);
   }
 }
+
+
+
+// SECOND ===========================
+let num =0
+let clicks = document.getElementById('counter2')
+let timeOut
+
+function counter2(){
+    clearTimeout(timeOut)
+    num ++
+    clicks.innerHTML = num
+}
+
+function resetCount(){
+  timeOut = setTimeout(function(){
+        num = 0
+        clicks.innerHTML = 0
+      
+    }, 4000);
+
+}
+
